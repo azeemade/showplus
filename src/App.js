03542885bcfab3e58cc508940260287246f4c278
Azeem Adenuga <adenugaazeem80@gmail.com>
2022-11-04 13:42:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Header } from './components/Header';
+import { Movies } from './components/Movie';
+import { Search } from './components/Search';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [movies, setMovies] = useState([]);
+  const [errormsg, setErrormsg] = useState("")
+  const [search, setSearch] = useState("");
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+
+    setLoading(true);
+
+    fetch(`https://www.omdbapi.com/?s=${search}&apikey=6145b6af`)
+    .then(response => response.json())
+    .then(jsonResponse => {
+      if (jsonResponse.Response === "True") {
+        setMovies(jsonResponse.Search);
+        setLoading(false);
+      } else {
+        setErrormsg(jsonResponse.Error);
+        setLoading(false);
+      }
+    });
+  };
+
+  const handleClear = () => {
+    setMovies([])
+    setSearch("")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='items-center justify-start flex flex-col'>
+      <div className='w-[600px]'>
+        <Header title="Showplus"/>
+        <Search search={search} handleChange={handleChange} />
+        <Movies movies={movies} loading={loading} />
+        {
+          movies.length > 0 &&
+            <div className="flex justify-center items-center mt-2 mb-5">
+              <button className="py-2 px-6 bg-neutral-500 text-white rounded border-neutral-500 text-sm" 
+                  type="submit" onClick={handleClear}>Clear All Data</button>
+            </div>
+        }
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export const Header = ({title}) => {
+    return (
+        <>
+            <header className="h-12 bg-neutral-50 border border-neutral-200 rounded-t-lg py-4 flex justify-center items-center my-4">
+                <h2 className="text-neutral-500 font-semibold font-instagram text-2xl">{title}</h2>
+            </header>
+        </>
+    )
+}
